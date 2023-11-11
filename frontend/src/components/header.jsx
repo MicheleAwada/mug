0278005cbe5bdb6b163e6b249827affb5dce2676
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-11-11 17:41:07 +0300

    modern profile menu view in header

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,12 +1,24 @@
 import Mug_Logo from "../assets/mug bg.png";
 import { Link, NavLink } from "react-router-dom";
 
+import { Dropdown } from "flowbite-react";
+
+import { IoSettingsOutline } from "react-icons/io5";
+import { HiOutlineNewspaper } from "react-icons/hi2";
+import { HiOutlineLogout } from "react-icons/hi";
+import { CgProfile } from "react-icons/cg";
+
 export default function Header({ context }) {
 	const [isAuthenticated] = context.auth;
 	const [currentUser] = context.user;
+	console.log("YEMPEJ");
+	console.log(currentUser);
 	return (
 		<div>
-			<header id="header" className="bg-gray-200 fixed w-full h-20 md:h-16">
+			<header
+				id="header"
+				className="bg-gray-200 fixed w-full h-20 md:px-6 md:h-16 z-10"
+			>
 				<div className="flex justify-between items-center h-full">
 					{/* <div className="flex items-center h-full"> */}
 					<div className="flex items-center h-full">
@@ -24,27 +36,57 @@ export default function Header({ context }) {
 								Login
 							</Link>
 							<Link
-								to="#"
+								to="/signup/"
 								className="bg-cyan-600 hover:bg-cyan-700 active:bg-cyan-800 text-cyan-100 mx-4 rounded-sm box-border px-6 py-2"
 							>
 								Signup
 							</Link>
 						</nav>
 					) : (
-						<nav>
-							{/* TODO add Navlink working */}
-							<Link
-								to="logout/"
-								className="bg-amber-600 hover:bg-amber-700 active:bg-amber-800 text-amber-100 mx-4 rounded-sm box-border px-6 py-2"
+						<nav className="h-full">
+							<Dropdown
+								label=""
+								dismissOnClick={true}
+								renderTrigger={() => (
+									<img
+										src={currentUser.avatar}
+										alt="Profile Logo"
+										className="p-2 h-full rounded-none"
+									/>
+								)}
 							>
-								Logout
-							</Link>
-							<Link
-								to="posts/create/"
-								className="bg-cyan-600 hover:bg-cyan-700 active:bg-cyan-800 text-cyan-100 mx-4 rounded-sm box-border px-6 py-2"
-							>
-								Create Post
-							</Link>
+								<Dropdown.Header>
+									<span className="block text-sm">
+										<span className="bold">Hello, </span>
+										{currentUser.username}
+									</span>
+								</Dropdown.Header>
+								<Dropdown.Item as={Link} className="flex items-center" href="#">
+									<CgProfile className="w-4 h-4 mr-2" />
+									Profile
+								</Dropdown.Item>
+								<Dropdown.Item
+									as={Link}
+									className="flex items-center"
+									href="/posts/create/"
+								>
+									<HiOutlineNewspaper className="w-4 h-4 mr-2" />
+									Posts
+								</Dropdown.Item>
+								<Dropdown.Item as={Link} className="flex items-center" href="#">
+									<IoSettingsOutline className="w-4 h-4 mr-2" />
+									Settings
+								</Dropdown.Item>
+								<Dropdown.Divider />
+								<Dropdown.Item
+									as={Link}
+									className="flex items-center"
+									href="/logout/"
+								>
+									<HiOutlineLogout className="w-4 h-4 mr-2" />
+									Logout
+								</Dropdown.Item>
+							</Dropdown>
 						</nav>
 					)}
 				</div>
