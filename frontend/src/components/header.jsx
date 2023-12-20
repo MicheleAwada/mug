716087f1bfe6b4db390fd0ac43504c3c009bc03b
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-12-20 19:34:44 +0200

    added beatiful menu

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,105 +1,205 @@
 import Mug_Logo from "../assets/mug bg.png";
-import { Link, NavLink } from "react-router-dom";
+import { Form, Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import { Dropdown } from "flowbite-react";
 
-import { IoSettingsOutline } from "react-icons/io5";
+import { TbListDetails } from "react-icons/tb";
+import { IoLogIn, IoLogInSharp, IoSettingsOutline } from "react-icons/io5";
+import { MdOutlineSecurity } from "react-icons/md";
 import { HiOutlineNewspaper } from "react-icons/hi2";
-import { HiOutlineLogout, HiOutlinePencil } from "react-icons/hi";
-import { CgProfile } from "react-icons/cg";
+import { HiLogin, HiOutlineLogin, HiOutlineLogout, HiOutlinePencil } from "react-icons/hi";
+import { CgDetailsMore, CgLogIn, CgProfile } from "react-icons/cg";
+import { Twirl as Hamburger } from 'hamburger-react'
 
 export default function Header({ context }) {
 	const [isAuthenticated] = context.auth;
 	const [currentUser] = context.user;
+
+	const [toggleHamburger, setToggleHamburger] = useState(false);
+	function toggle_hamburger() {
+		setToggleHamburger(!toggleHamburger);
+	}
+	useEffect(() => {
+		const menu = document.getElementById("menu");
+		if (toggleHamburger) {
+			menu.style.left = "0"
+		} else {
+			menu.style.left = "-40rem"
+		}
+
+		return () => {
+			menu.style.left = "-40rem"
+		}
+	}, [toggleHamburger])
+
+	const location = useLocation();
+
+	function isSelected(link) {
+		const lastLetter = link[link.length - 1];
+		link = (lastLetter==="/") && link.slice(0, -1) || link;
+		if (location.pathname.startsWith(link)) {
+			return true;
+		}
+		return false;
+	}
+
+	let menu_map = [
+	]
+	if (isAuthenticated) {
+		menu_map = [
+			{
+				title: "Profile",
+				icon: CgProfile,
+				link: `/author/${currentUser.id}/`,
+			},
+			{
+				title: "Create Post",
+				icon: HiOutlineNewspaper,
+				link: "/posts/create/",
+			},
+			{
+				title: "Account Details",
+				icon: TbListDetails,
+				link: "/profile/details/",
+			},
+			{
+				title: "Account Security",
+				icon: MdOutlineSecurity,
+				link: "/profile/security/",
+			},
+			{
+				title: "Logout",
+				icon: HiOutlineLogout,
+				link: "/logout/",
+			},
+		]
+	} else {
+		menu_map = [
+			{
+				title: "Log in",
+				icon: HiLogin,
+				link: "/login/",
+			},
+			{
+				title: "Sign Up",
+				icon: IoLogInSharp,
+				link: "/signup/",
+			},
+		]
+	}
+
 	return (
 		<div>
-			<header
-				id="header"
-				className="bg-gray-200 fixed w-full h-20 md:px-6 md:h-16 z-10"
-			>
-				<div className="flex justify-between items-center h-full">
-					{/* <div className="flex items-center h-full"> */}
-					<div className="flex items-center h-full">
-						<Link to="/" className="h-full p-2 pl-6">
-							<img src={Mug_Logo} alt="Logo" className="h-full" />
-						</Link>
-						<p className="nunito text-2xl">Mug</p>
-					</div>
-					{!isAuthenticated ? (
-						<nav className="flex">
-							<Link
-								to="/login/"
-								className="bg-amber-600 hover:bg-amber-700 active:bg-amber-800 text-amber-100 mx-4 rounded-sm box-border px-6 py-2"
-							>
-								Login
-							</Link>
-							<Link
-								to="/signup/"
-								className="bg-cyan-600 hover:bg-cyan-700 active:bg-cyan-800 text-cyan-100 mx-4 rounded-sm box-border px-6 py-2"
-							>
-								Signup
-							</Link>
-						</nav>
-					) : (
-						<nav className="h-full">
-							<Dropdown
-								label=""
-								dismissOnClick={true}
-								renderTrigger={() => (
-									<div className="h-full p-2">
-										<img
-											src={currentUser.avatar}
-											alt="Profile Logo"
-											className=" h-full rounded-md cursor-pointer"
-										/>
-									</div>
-								)}
-							>
-								<Dropdown.Header>
-									<span className="block text-sm">
-										Hello, <span className="bold">{currentUser.username}</span>
-									</span>
-								</Dropdown.Header>
-								<Dropdown.Item to="/profile/" as={Link} className="flex items-center">
-									<CgProfile className="w-4 h-4 mr-2" />
-									Profile
-								</Dropdown.Item>
-								<Dropdown.Item
-									as={Link}
-									className="flex items-center"
-									to={`/posts/create/`}
-								>
-									<HiOutlinePencil className="w-4 h-4 mr-2" />
-									Create Post
-								</Dropdown.Item>
-								<Dropdown.Item
-									as={Link}
-									className="flex items-center"
-									to={`/author/${currentUser.id}/`}
-								>
-									<HiOutlineNewspaper className="w-4 h-4 mr-2" />
-									Posts
-								</Dropdown.Item>
-								{/* <Dropdown.Item as={Link} className="flex items-center" to="#">
-									<IoSettingsOutline className="w-4 h-4 mr-2" />
-									Settings
-								</Dropdown.Item> */}
-								<Dropdown.Divider />
-								<Dropdown.Item
-									as={Link}
-									className="flex items-center"
-									to="/logout/"
-								>
-									<HiOutlineLogout className="w-4 h-4 mr-2" />
-									Logout
-								</Dropdown.Item>
-							</Dropdown>
-						</nav>
-					)}
-				</div>
-			</header>
 			<div id="fake-header" className="bg-transparent w-full h-20 md:h-16">
 				{/* this is so content wont get clipped */}
+			</div>
+				
+			<div className="flex flex-col absolute top-0 left-0 h-screen">
+				<div className="">
+					<header
+						id="header"
+						className="bg-gray-300 fixed w-full h-20 md:px-6 md:h-16 z-40"
+					>
+						<div className="flex justify-between items-center h-full">
+							{/* <div className="flex items-center h-full"> */}
+							<div className="flex items-center h-full">
+								<Hamburger size={20} color="black" direction="left" toggled={toggleHamburger} toggle={toggle_hamburger} />
+								<Link to="/" className="h-full p-2 pl-6">
+									<img src={Mug_Logo} alt="Logo" className="h-full" />
+								</Link>
+								<p className="nunito text-2xl">Mug</p>
+							</div>
+							{!isAuthenticated ? (
+								<nav className="flex">
+									<Link
+										to="/login/"
+										className="bg-amber-600 hover:bg-amber-700 active:bg-amber-800 text-amber-100 mx-4 rounded-md box-border px-6 py-2"
+									>
+										Login
+									</Link>
+									{/* <Link
+										to="/signup/"
+										className="bg-cyan-600 hover:bg-cyan-700 active:bg-cyan-800 text-cyan-100 mx-4 rounded-sm box-border px-6 py-2"
+									>
+										Signup
+									</Link> */}
+								</nav>
+							) : (
+								<nav className="h-full">
+									<Dropdown
+										label=""
+										dismissOnClick={true}
+										renderTrigger={() => (
+											<div className="h-full p-2">
+												<img
+													src={currentUser.avatar}
+													alt="Profile Logo"
+													className=" h-full rounded-md cursor-pointer"
+												/>
+											</div>
+										)}
+									>
+										<Dropdown.Header>
+											<span className="block text-sm">
+												Hello, <span className="bold">{currentUser.username}</span>
+											</span>
+										</Dropdown.Header>
+										<Dropdown.Item
+											as={Link}
+											className="flex items-center"
+											to={`/author/${currentUser.id}/`}
+										>
+											<CgProfile className="w-4 h-4 mr-2" />
+											Profile
+										</Dropdown.Item>
+										<Dropdown.Item
+											as={Link}
+											className="flex items-center"
+											to={`/posts/create/`}
+										>
+											<HiOutlinePencil className="w-4 h-4 mr-2" />
+											Create Post
+										</Dropdown.Item>
+										<Dropdown.Item as={Link} className="flex items-center" to="/profile/">
+											<IoSettingsOutline className="w-4 h-4 mr-2" />
+											Settings
+										</Dropdown.Item>
+										<Dropdown.Divider />
+										<Dropdown.Item
+											as={Link}
+											className="flex items-center"
+											to="/logout/"
+										>
+											<HiOutlineLogout className="w-4 h-4 mr-2" />
+											Logout
+										</Dropdown.Item>
+									</Dropdown>
+								</nav>
+							)}
+						</div>
+					</header>
+					<div id="fake-header2" className="bg-transparent w-full h-20 md:h-16">
+						{/* this is so content wont get clipped */}
+					</div>
+				</div>
+				<div id="menu" className="bg-gray-200 z-30 fixed py-4">
+					<ul className="w-full ">
+						{menu_map.map((info, index) => {
+							const isSelectedItem = isSelected(info.link)
+							return (<li key={index} className="w-full px-4 flex flex-col items-center">
+								<Link to={info.link} className={"px-4 py-2 rounded-md flex items-center gap-4 w-full h-full " + (isSelectedItem ? "border-gray-300 border-4" : "bg-gray-300 border-transparent border-4")}>
+									{info.src && <img src={info.src} className="object-cover w-10 h-10 rounded-full"/>}
+									{info.icon && <info.icon color="#333" className="object-cover w-8 h-8 rounded-full"/>}
+									<p className="text-center text-gray-950 font-medium text-lg">{info.title}</p>
+								</Link>
+								<div className="py-4">
+									{index!==menu_map.length-1 && <div className="px-14 border-b-2 border-gray-600" />}
+								</div>
+							</li>)
+						})}
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
