d28b12e18de38f1a03724bee207e935af3cde84b
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-12-26 10:08:54 +0200

    prod for vps

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		host: true,
-		port: "80",
-		watch: {
-			usePolling: true,
-		}
+	build: {
+		watch: false,
 	},
 });
