4170d23864ceda49bacc0cb4e33dbe3cbadc2a54
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-01 20:27:08 +0200

    removed ring offest bug in button comp

--- frontend/src/components/button.jsx
+++ frontend/src/components/button.jsx
@@ -1,14 +1,14 @@
 import Loading from "./loading";
 
-export default function Button({ children, color="amber", loading=false, className="", as="button" ...props }) {
-    let classes = "flex items-center justify-center gap-2 my-6 py-1 px-4 rounded-md ring-0 ring-offset-2 active:ring-2 ";
+export default function Button({ children, color="amber", loading=false, className="", as="button", ...props }) {
+    let classes = "flex items-center justify-center gap-2 my-6 py-1 px-4 rounded-md ring-0 active:ring-2  ";
     classes += `text-white bg-${color}-600 hover:bg-${color}-700 active:ring-${color}-400`
 
-    const Element = as
+    const Element = as;
 
     return (
         <Element {...props}
-            className={className+" "+classes+""}
+            className={className+" "+classes+" "}
         >
             <Loading show={loading} />
             {children}
