8a5a633318bcedf712d1b4647e5bb050fc7652c6
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-02 13:43:01 +0200

    buttons working with tailwind

--- frontend/src/components/button.jsx
+++ frontend/src/components/button.jsx
@@ -1,14 +1,23 @@
 import Loading from "./loading";
 
 export default function Button({ children, color="amber", loading=false, className="", as="button", ...props }) {
-    let classes = "h-8 flex items-center justify-center gap-2 py-1 px-4 rounded-md ring-0 active:ring-2  ";
-    classes += `text-white bg-${color}-600 hover:bg-${color}-700 active:ring-${color}-400`
 
     const Element = as;
 
     return (
         <Element {...props}
-            className={className+" "+classes+" "}
+            className={(color==="cyan" &&
+            `h-8 flex items-center justify-center gap-2 py-1 px-4 rounded-md ring-0 active:ring-2 text-white bg-cyan-600 hover:bg-cyan-700 active:ring-cyan-400`
+            || color==="amber" &&
+            `h-8 flex items-center justify-center gap-2 py-1 px-4 rounded-md ring-0 active:ring-2 text-white bg-amber-600 hover:bg-amber-700 active:ring-amber-400`
+            || color==="blue" &&
+            `h-8 flex items-center justify-center gap-2 py-1 px-4 rounded-md ring-0 active:ring-2 text-white bg-blue-600 hover:bg-blue-700 active:ring-blue-400`
+            || color==="gray" &&
+            `h-8 flex items-center justify-center gap-2 py-1 px-4 rounded-md ring-0 active:ring-2 text-white bg-gray-600 hover:bg-gray-700 active:ring-gray-400`
+            || color==="white" &&
+            `h-8 flex items-center justify-center gap-2 py-1 px-4 rounded-md ring-0 active:ring-2 text-white bg-white-600 hover:bg-white-700 active:ring-white-400`
+            
+            )}
         >
             <Loading show={loading} />
             {!loading && children}
