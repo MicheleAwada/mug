d7d11746e9739629e9c31b241c79bf1a3d36630c
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-02 13:54:07 +0200

    more responsive button

--- frontend/src/components/button.jsx
+++ frontend/src/components/button.jsx
@@ -7,17 +7,17 @@ export default function Button({ children, color="amber", loading=false, classNa
     return (
         <Element {...props}
             className={(color==="cyan" &&
-            `h-8 flex items-center justify-center gap-2 py-1 px-4 rounded-md ring-0 active:ring-2 text-white bg-cyan-600 hover:bg-cyan-700 active:ring-cyan-400`
+            `text-white bg-cyan-600 hover:bg-cyan-700 active:ring-cyan-400`
             || color==="amber" &&
-            `h-8 flex items-center justify-center gap-2 py-1 px-4 rounded-md ring-0 active:ring-2 text-white bg-amber-600 hover:bg-amber-700 active:ring-amber-400`
+            `text-white bg-amber-600 hover:bg-amber-700 active:ring-amber-400`
             || color==="blue" &&
-            `h-8 flex items-center justify-center gap-2 py-1 px-4 rounded-md ring-0 active:ring-2 text-white bg-blue-600 hover:bg-blue-700 active:ring-blue-400`
+            `text-white bg-blue-600 hover:bg-blue-700 active:ring-blue-400`
             || color==="gray" &&
-            `h-8 flex items-center justify-center gap-2 py-1 px-4 rounded-md ring-0 active:ring-2 text-white bg-gray-600 hover:bg-gray-700 active:ring-gray-400`
+            `text-white bg-gray-600 hover:bg-gray-700 active:ring-gray-400`
             || color==="white" &&
-            `h-8 flex items-center justify-center gap-2 py-1 px-4 rounded-md ring-0 active:ring-2 text-white bg-white-600 hover:bg-white-700 active:ring-white-400`
+            `text-white bg-gray-300 hover:bg-gray-400 active:ring-gray-100`
             
-            )
+            ) + " " + "h-8 flex items-center justify-center gap-2 py-3 px-5 lg:py-4 lg:px-6 rounded-md ring-0 active:ring-2"
             + " " + className
         }
         >
